fdeb0597394dadfaf4178b7771e5f242d6a77f94
Greg Salisbury <109830514+Gregrs400@users.noreply.github.com>
2022-12-04 11:54:22 -0500

    Add files via upload

--- Passenger.java
+++ Passenger.java
@@ -12,59 +12,82 @@ public class Passenger extends Person
 
     private ArrayList<Integer> bags = new ArrayList<>();
 
+    //generatePassengerBags to assign a Passenger object bags
+
+    public ArrayList<Integer> generatePassengerBags()
+    {//begin getPassengerBags
+
+        ArrayList<Integer> passengerBags = new ArrayList<>();
+
+        //assigning random int between 1 and 3 inclusive, determining the amount of times the inner loop will run, and the number of bags that will be created
+
+        int numOfBags = random.nextInt(3) + 1;
+
+        //inner loop, "j" representing the creation of a passenger's bag, and the element of that bag in the bags ArrayList
+        //bagWeight represents the weight of a passenger's bag in pounds, 50 being the upper limit. All bags are between 20 and 50 pounds inclusive at the moment
+
+        for (int i = 0; i < numOfBags; i++)
+        {//begin for loop to generate bag weights
+
+            int bagWeight;
+
+            bagWeight = random.nextInt(31)+20;
+
+            passengerBags.add(bagWeight);
+
+        }//end for loop to generate bag weights
+
+        return passengerBags;
+
+    }//end getPassengerBags
+
+    public ArrayList<Integer> getBags() {return bags;}
+
+    public void setBags(ArrayList<Integer> bags) {this.bags = bags;}
+
     //placeholder int variable for Flight Numbers before they are passed in during passenger construction
 
     private int flightNumber = 0;
 
+    public int getFlightNumber() {return flightNumber;}
+
     private String destination;
 
-    private int curbToCheckIn, checkInToSecurity, securityToGate, gateToPlane, eatingTime;
+    public String getDestination() {return destination;}
+
+    private int curbToCheckIn, checkInToSecurity, securityToGate, gateToPlane;
+
+    public int getCurbToCheckIn() {return curbToCheckIn;}
+
+    public int getCheckInToSecurity() {return checkInToSecurity;}
+
+    public int getSecurityToGate() {return securityToGate;}
+
+    public int getGateToPlane() {return gateToPlane;}
 
     private boolean atGate = false;
 
+    public boolean isAtGate() {return atGate;}
 
+    public void setAtGate(boolean atGate) {this.atGate = atGate;}
 
-    public ArrayList<Integer> getBags() {
-        return bags;
-    }
+    enum airportTravel
+    {//begin airportTravel enum
 
-    public void setBags(ArrayList<Integer> bags) {
-        this.bags = bags;
-    }
+        DROPPED_OFF,
+        CHECKING_IN,
+        CHECKED_IN,
+        AT_SECURITY,
+        THROUGH_SECURITY,
+        WALKING_TO_GATE,
+        AT_GATE,
+        BOARDING_PLANE
 
-    public int getFlightNumber() {
-        return flightNumber;
-    }
-
-    public void setFlightNumber(int flightNumber) {
-        this.flightNumber = flightNumber;
-    }
-
-    public String getDestination() {
-        return destination;
-    }
-
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
-
-    public int getCurbToCheckIn() {return curbToCheckIn; }
-
-    public int getCheckInToSecurity() {return checkInToSecurity; }
-
-    public int getSecurityToGate() {return securityToGate; }
-
-    public int getGateToPlane() {return gateToPlane; }
-
-    public boolean isAtGate() {
-        return atGate;
-    }
-
-    public void setAtGate(boolean atGate){this.atGate = atGate;}
+    }//end airportTravel enum
 
     airportTravel at;
 
-    Gate gate;
+    private Gate gate;
 
 
     //Passenger parameterized constructor, assigning each passenger a flight number, an id, and 1-3 bags inclusive
@@ -75,7 +98,7 @@ public class Passenger extends Person
         this.flightNumber = flightNumber;
         this.destination = destination;
         setId(id);
-        bags = getPassengerBags();
+        bags = generatePassengerBags();
         commuteThroughAirport();
         this.gate = gate;
 
@@ -89,54 +112,17 @@ public class Passenger extends Person
 
     }//end Passenger parameterized constructor
 
+    //Passenger no args constructor to access values of a passenger
+
     public Passenger()
     {//begin Passenger default no args constructor
 
     }//end Passenger default no args constructor
 
-    enum airportTravel
-    {
-
-        DROPPED_OFF,
-        CHECKING_IN,
-        CHECKED_IN,
-        AT_SECURITY,
-        THROUGH_SECURITY,
-        WALKING_TO_GATE,
-        AT_GATE,
-        EATING,
-        BOARDING_PLANE
-
-    }
-    public ArrayList<Integer> getPassengerBags()
-    {//begin getPassengerBags
-
-        ArrayList<Integer> passengerBags = new ArrayList<>();
-
-        //assigning random int between 1 and 3 inclusive, determining the amount of times the inner loop will run, and the number of bags that will be created
-
-        int jCounter = random.nextInt(3) + 1;
-
-        //inner loop, "j" representing the creation of a passenger's bag, and the element of that bag in the bags ArrayList
-        //bagWeight represents the weight of a passenger's bag in pounds, 50 being the upper limit. All bags are between 20 and 50 pounds inclusive at the moment
-
-        for (int j = 0; j < jCounter; j++)
-        {//begin inner for loop
-
-            int bagWeight;
-
-            bagWeight = random.nextInt(30)+20;
-
-            passengerBags.add(bagWeight);
-
-        }//end inner for loop
-
-        return passengerBags;
-
-    }//end getPassengerBags
+    //printPassenger to print information about a passenger
 
     public void printPassenger()
-    {
+    {//begin printPassenger
 
         System.out.println("\nPassenger Flight Number: " + flightNumber);
 
@@ -145,69 +131,78 @@ public class Passenger extends Person
         System.out.println("Passenger destination: " + destination);
 
         for(int i = 0; i < bags.size(); i++)
-        {
+        {//begin for loop to print all passenger's bag weights
 
             System.out.println("Bag " + (i+1) + " Weight: " + bags.get(i) + " pounds");
 
-        }
+        }//end for loop to print all passenger's bag weights
 
-    }
+    }//end printPassenger
+
+    /* commuteThroughAirport generates durations for each stage of
+    a passenger getting from the curb to the gate of their airport */
 
     public void commuteThroughAirport()
-    {
+    {//begin commuteThroughAirport
 
         curbToCheckIn = random.nextInt(11)+5;
         checkInToSecurity = random.nextInt(6)+5;
         securityToGate = random.nextInt(31)+15;
         gateToPlane = random.nextInt(11)+5;
 
-    }
+    }//end commuteThroughAirport
+
+    /* movePassenger to get passengers to their gate through decrementing
+    the above variables and switching enums when each field equals 0. The method
+    is called for a passenger while they have not made it to their gate. */
 
     public void movePassenger()
-    {
+    {//begin movePassenger
 
         if(at.equals(airportTravel.DROPPED_OFF))
-        {
+        {//begin if passenger has been dropped off but not checked in
 
             curbToCheckIn--;
 
             if(curbToCheckIn == 0)
-            {
+            {//begin if passenger has made it to check in
 
                 at = airportTravel.CHECKED_IN;
 
-            }
+            }//end if passenger has made it to check in
 
-        }
+        }//end if passenger has been dropped off but not checked in
 
         if(at.equals(airportTravel.CHECKED_IN))
-        {
+        {//begin if passenger has checked in but not been through security
 
             checkInToSecurity--;
 
             if(checkInToSecurity == 0)
-            {
+            {//begin if passenger gets through security
 
                 at = airportTravel.THROUGH_SECURITY;
 
-            }
+            }//end if passenger gets through security
 
-        }
+        }//end if passenger has checked in but not been through security
 
         if(at.equals(airportTravel.THROUGH_SECURITY))
-        {
+        {//begin if passenger is through security but not gotten to gate
 
             securityToGate--;
 
-        }
+        }//end if passenger is through security but not gotten to gate
 
-    }
+    }//end movePassenger
+
+    //prints the enum currently set to the Passenger object
 
     public void printPaxAirportStatus()
-    {
+    {//begin printPaxAirportStatus
 
         System.out.println(at);
 
-    }
+    }//end printPaxAirportStatus
 
 }//end Passenger class
